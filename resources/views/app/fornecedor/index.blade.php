ae11da5739fa891d8e7c078fcc63d008d7a8a62c
browsing24 <57516562+AndersonD4vid@users.noreply.github.com>
2023-08-13 19:29:10 -0300

    chore: Blade @if/@else

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -6,5 +6,15 @@
 {{-- Aqui fica o comentário que será descartado pelo interpretador do blade --}}
 
 @php
-    // Aqui é um comentário php puro
+
 @endphp
+
+{{-- @dd($fornecedores) --}}
+
+@if(count($fornecedores) > 0 && count ($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados aqui.</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados aqui.</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados aqui.</h3>
+@endif
