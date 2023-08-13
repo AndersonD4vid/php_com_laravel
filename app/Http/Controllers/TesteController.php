46652c6d5e6aeec1a39941cb8e0d75033113a47e
browsing24 <57516562+AndersonD4vid@users.noreply.github.com>
2023-08-13 19:11:24 -0300

    chore: encaminhando parâmetros do controlador para determinada view
    Para isso temos algumas possibilidades, tais como Array associativo, Compact() que é um método nativo do php que na prática cria um array associativo baseado em parêmetros. O With() é um método do laravel. Na prática iremos usar mais o  método compact()

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste(int $p1, int $p2){
-        echo "A soma de $p1 + $p2 é = ".($p1+$p2);
+        //echo "A soma de $p1 + $p2 é = ".($p1+$p2);
+
+        /* Método: Array associativo
+            return view('site.teste', ['p1' => $p1, 'p2' => $p2]);
+        */
+
+        /* Método: Compact
+            Explicaçao básica: O indice será o nome da variável exemplo 'p1', e o valor atribuido a esse indice será o valor contido na variável
+            return view('site.teste', compact('p1', 'p2'));
+        */
+
+        // Método: With
+        return view('site.teste')->with('p1', $p1)->with('p2', $p2);
     }
 }
