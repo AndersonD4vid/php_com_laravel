608e6f9541aa6f0182da9ccf96acac2ba736bee4
browsing24 <57516562+AndersonD4vid@users.noreply.github.com>
2023-08-13 19:53:54 -0300

    chore: blade @unless
    O operador @unless é uma inversão do operador if, enquanto o if analisa o retorno positivo de uma condição o @unless faz o inverso. O @unless não tem uma correspondência direta com um comando da linguagem de programação php. O unless funciona mais como um atalho.

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,20 +1,33 @@
 <h2>Fornecedor</h2>
 
-{{ 'Texto de teste' }}
-
-
-{{-- Aqui fica o comentário que será descartado pelo interpretador do blade --}}
-
-@php
-
-@endphp
 
 {{-- @dd($fornecedores) --}}
 
-@if(count($fornecedores) > 0 && count ($fornecedores) < 10)
+Fornecedor: {{$fornecedores[0]['nome']}} <br/>
+Status: {{$fornecedores[0]['status']}}
+
+<br/>
+
+@if( !($fornecedores[0]['status'] == 'S'))
+    Fornecedor inativo
+@endif
+
+<br/>
+
+{{-- O @unless --}}
+@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
+  Fornecedor inativo
+@endunless
+
+<br/>
+
+{{-- @if(count($fornecedores) > 0 && count ($fornecedores) < 10)
     <h3>Existem alguns fornecedores cadastrados aqui.</h3>
 @elseif(count($fornecedores) > 10)
     <h3>Existem vários fornecedores cadastrados aqui.</h3>
 @else
     <h3>Ainda não existem fornecedores cadastrados aqui.</h3>
-@endif
+@endif --}}
+
+
+
